295c52d04ef3f11924eb5378405b4bb2ac1f5203
Long ai/λ sqrt(r^2+(xl-xc)^2) <74458457+toohandsomelong@users.noreply.github.com>
2025-01-08 12:23:05 +0700

    First Commit

---
+++ QuickPythagoras/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace QuickPythagoras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0;
+            float b = 0;
+            
+            while (true)
+            {
+                Console.Write("a = ");
+                while (!(float.TryParse(Console.ReadLine(), out a)))
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.Write("a = ");
+                }
+                Console.Write("b = ");
+                while (!(float.TryParse(Console.ReadLine(), out b)))
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.Write("b = ");
+                }
+
+                Console.WriteLine(Math.Sqrt(a * a + b * b));
+                Console.WriteLine("Press Ctrl + C to end");
+            }
+        }
+    }
+}
